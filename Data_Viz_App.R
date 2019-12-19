e71ea64506cf693162f570b17dfe6a8ac905c558
Harsh Sharda <hs957@georgetown.edu>
2019-12-19 14:07:27 -0500

    adding the geo plot

--- Data_Viz_App.R
+++ Data_Viz_App.R
@@ -6,13 +6,27 @@ library(gapminder)
 library(plotly)
 library(ggplot2)
 library(scales)
-library(ggrepel)
+library(dplyr)
+library(leaflet)
+library(sf)
+library(htmltools)
 
 
 # Import Dataset
 
 country_agg_data <- read.csv('Data/Powerplant/gdp_emissions_pop.csv')
 powerplant_viz <- read.csv("Data/Powerplant/power_plant_data_locations.csv")
+
+
+#powerplant_viz <- read.csv("Data/Powerplant/power_plant_data_locations.csv")
+powerplant_emissions <- read.csv("Data/Powerplant/power_plants_locations_emissions.csv")
+map_world <- st_read("Data/Shapefile/TM_WORLD_BORDERS-0.3.shp")
+
+#country_data <- read.csv("Data/DataViz memo_hs957.csv")
+
+# Data Wrangling Tasks
+
+## For Sunbursts charts
 
 check1 <- powerplant_viz %>%
   group_by(country_name,fuel_category,primary_fuel) %>%
@@ -25,6 +39,7 @@ grandTotals <- check1 %>%
   rename(country_name = Name) %>%
   bind_rows(check1)
 
+## For GDP PPP vs Emissions Data
 
 ylab <- c(0, 2.5, 5.0, 7.5, 10, 12.5)
 xlab <- c(0, 2.5, 5.0, 7.5 ,10.0)
@@ -35,21 +50,39 @@ country_agg_data$country_col <- country_agg_data$country_name
 ls <- grandTotals %>% spread(primary_fuel, tot_capacity, fill = 0) %>%
   names(.) %>% .[3:17]
 
+## For Geoplotting Powerplants
 
+## Modifying Shape file to match with the Power Plants Data
+
+map_world <- subset(map_world,is.element(map_world$ISO3,powerplant_emissions$ISO3))
+powerplant_emissions <- subset(powerplant_emissions,is.element(powerplant_emissions$ISO3,map_world$ISO3)) %>%
+  .[order(match(powerplant_emissions$ISO3,map_world$ISO3)),]
+
+## Creating bins and palettes
+bins <- c(0, 2, 4, 6, 8, 10, 12, Inf)
+pal <- colorBin(palette = "YlOrRd",
+                domain = powerplant_emissions$emissions_total_2014,
+                bins = bins * 10^6)
+pal2 <- colorFactor(palette = c("firebrick3","darkolivegreen3"), domain = powerplant_viz$fuel_category)
+
+### Adding labels to the Emissions polygons
+label <- paste("<p>","Country Name: ",powerplant_emissions$country_name,"</p>",
+               "<p>","Emissions: ",powerplant_emissions$emissions_total_2014,"</p>",
+               sep = "")
 
 # Define Country List
 
-## Visualization 1:
+## Visualization 1 (GDP PPP vs Emissions Data):
 country_list_viz1 <- distinct(country_agg_data, country_name)
 country_list_viz1 <- as.list(levels(country_list_viz1$country_name))
 country_list_viz1 <- c('All',country_list_viz1)
 
-## Visualization 2:
+## Visualization 2 (Sunbursts - Left):
 country_list_viz2 <- distinct(check1, country_name)
 country_list_viz2 <- as.list(levels(country_list_viz2$country_name))
 country_list_viz2 <- c('All',country_list_viz2)
 
-## Visualization 3:
+## Visualization 3 (Sunbursts - Right):
 country_list_viz3 <- c('All',country_list_viz2)
 
 
@@ -85,6 +118,19 @@ ui <- fluidPage(
       )
     
     ),
+  hr(),
+  
+  fluidRow(
+    column(width = 12, align = "center",
+    h4("Spread of Powerplants throughout the world")),
+    column(width = 12,
+    p("The world is rapidly changing. It is important to understand how different countries' development trajectories have changed over time.
+      This dashboard is intended to make it easier to see trends for individual countries as well as groups of countries.
+      Press the play button below to see how countries have progressed over time.
+      Select a country from the dropdown list to highlight its progress."),
+    leafletOutput("geo_plot"))
+    ),
+  
   hr(),
   
   sidebarPanel(
@@ -157,6 +203,36 @@ server <- function(input, output){
     }
   })
   
+  
+  output$geo_plot <- renderLeaflet({
+    
+    m <- leaflet(powerplant_viz) %>%  addTiles() %>%
+      addPolygons(data = map_world, weight = 1, smoothFactor = 0.5, color = "white",
+                  fillOpacity = 0.8, fillColor = ~pal(powerplant_emissions$emissions_total_2014),
+                  label = lapply(label, HTML),
+                  group = "GHG Emissions") %>%
+      addLegend(pal = pal,
+                values = powerplant_emissions$emissions_total_2014,
+                opacity = 0.7,
+                position = "topright",
+                title = "GHG Emissions") %>%
+      addLegend(pal = pal2,
+                values = powerplant_viz$fuel_category,
+                opacity = 0.7,
+                position = "bottomright",
+                title = "Fuel Category") %>%
+      addCircleMarkers(lat = ~latitude, lng = ~longitude,
+                       popup = ~c(name),
+                       radius = ~0.0000001,
+                       color = ~pal2(powerplant_viz$fuel_category),
+                       fillOpacity = 0.8,
+                       group = "Power Plants") %>%
+      addLayersControl(overlayGroups = c("GHG Emissions", "Power Plants"),
+                       options = layersControlOptions(collapsed = FALSE),
+                       position = "bottomleft")
+    m
+    
+  })
   
 
   output$plot_sunburst <- renderPlotly({
